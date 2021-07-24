dffb4070262f8ef3a169b87cc0a4d29fbecfb97c
Ryota Nishi <ryota0312@s.okayama-u.ac.jp>
2021-07-24 10:28:36 +0900

    Modify network structure (proxy to Next.js use Gin server)

--- server/src/main.go
+++ server/src/main.go
@@ -6,22 +6,44 @@ import (
 	"github.com/gin-gonic/gin"
 	"hoshi-atsume-server/src/controller"
 	"log"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
 )
 
 func main() {
 	serve()
 }
 
-func serve()  {
+func serve() {
 	router := gin.Default()
 	store, _ := redis.NewStore(10, "tcp", "redis:6379", "", []byte("secret"))
 	router.Use(sessions.Sessions("session", store))
 
-	router.GET("/spots", controller.GetSpots)
+	api := router.Group("/api")
+	{
+		api.GET("/spots", controller.GetSpots)
+		api.POST("/auth", controller.Auth)
+	}
 
-	router.POST("/auth", controller.Auth)
+	// Proxy to Next.js
+	router.NoRoute(ReverseProxy)
 
 	if err := router.Run(":3001"); err != nil {
 		log.Fatal("Server Run Failed: ", err)
 	}
+}
+
+func ReverseProxy(c *gin.Context) {
+	remote, _ := url.Parse("http://host.docker.internal:3000")
+	proxy := httputil.NewSingleHostReverseProxy(remote)
+	proxy.Director = func(req *http.Request) {
+		req.Header = c.Request.Header
+		req.Host = remote.Host
+		req.URL = c.Request.URL
+		req.URL.Scheme = remote.Scheme
+		req.URL.Host = remote.Host
+	}
+
+	proxy.ServeHTTP(c.Writer, c.Request)
 }

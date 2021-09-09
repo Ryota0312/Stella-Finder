2f97ca60acbca67bc4aab477a280c16134d7ffc6
Ryota Nishi <ryota0312@s.okayama-u.ac.jp>
2021-09-10 07:20:19 +0900

    Add user feedback when register

--- ui/pages/register.tsx
+++ ui/pages/register.tsx
@@ -1,77 +1,103 @@
 import React, { useState } from 'react'
 import { useRouter } from 'next/router'
+import Link from 'next/link'
 import Layout from '../components/layout'
 
 const Register: React.FC = () => {
   const router = useRouter()
   const { registerKey } = router.query
 
+  const [isComplete, setIsComplete] = useState<boolean>(false)
+  const [error, setError] = useState<boolean>(false)
+
   const [loginName, setLoginName] = useState<string>('')
   const [userName, setUserName] = useState<string>('')
   const [password, setPassword] = useState<string>('')
   const [confirmPassword, setConfirmPassword] = useState<string>('')
 
-  return (
-    <Layout>
-      <main>
-        <h1>Register</h1>
+  if (isComplete) {
+    return (
+      <Layout>
+        <main>
+          <div>登録が完了しました！以下からログインしてください。</div>
+          <Link href={'login'}>ログイン</Link>
+        </main>
+      </Layout>
+    )
+  } else {
+    return (
+      <Layout>
+        <main>
+          <h1>Register</h1>
 
-        <div>{}</div>
+          {error && <div>エラーがあります</div>}
 
-        <div>
-          <p>ログイン名</p>
-          <input
-            type={'text'}
-            placeholder={'login name'}
-            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-              setLoginName(e.target.value)
-            }
-          />
-          <p>ユーザー名</p>
-          <input
-            type={'text'}
-            placeholder={'your e-mail address'}
-            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-              setUserName(e.target.value)
-            }
-          />
-          <p>パスワード</p>
-          <input
-            type={'password'}
-            placeholder={'password'}
-            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-              setPassword(e.target.value)
-            }
-          />
-          <p>確認パスワード</p>
-          <input
-            type={'password'}
-            placeholder={'confirm password'}
-            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-              setConfirmPassword(e.target.value)
-            }
-          />
-          {password !== confirmPassword && confirmPassword.length > 0 && (
-            <div>まちがってるよ</div>
-          )}
-          <button
-            type={'button'}
-            onClick={() =>
-              register_(
-                registerKey as string,
-                loginName,
-                userName,
-                password,
-                confirmPassword,
-              )
-            }
-          >
-            Register
-          </button>
-        </div>
-      </main>
-    </Layout>
-  )
+          <div>
+            <p>ログイン名</p>
+            <input
+              type={'text'}
+              placeholder={'login name'}
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                setLoginName(e.target.value)
+              }
+            />
+            <p>ユーザー名</p>
+            <input
+              type={'text'}
+              placeholder={'your e-mail address'}
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                setUserName(e.target.value)
+              }
+            />
+            <p>パスワード</p>
+            <input
+              type={'password'}
+              placeholder={'password'}
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                setPassword(e.target.value)
+              }
+            />
+            <p>確認パスワード</p>
+            <input
+              type={'password'}
+              placeholder={'confirm password'}
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                setConfirmPassword(e.target.value)
+              }
+            />
+            {password !== confirmPassword && confirmPassword.length > 0 && (
+              <div>まちがってるよ</div>
+            )}
+            <button
+              type={'button'}
+              onClick={() => {
+                register_(
+                  registerKey as string,
+                  loginName,
+                  userName,
+                  password,
+                  confirmPassword,
+                ).then(
+                  (res) => {
+                    if (res.ok) {
+                      setIsComplete(true)
+                    } else {
+                      setError(true)
+                    }
+                  },
+                  () => {
+                    setError(true)
+                  },
+                )
+              }}
+            >
+              Register
+            </button>
+          </div>
+        </main>
+      </Layout>
+    )
+  }
 }
 export default Register
 
@@ -83,7 +109,7 @@ const register_ = async (
   confirmPassword: string,
 ) => {
   if (password === confirmPassword) {
-    await fetch('/auth/register', {
+    return await fetch('/auth/register', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
@@ -97,5 +123,6 @@ const register_ = async (
     })
   } else {
     console.log('Password not match')
+    return Promise.reject()
   }
 }

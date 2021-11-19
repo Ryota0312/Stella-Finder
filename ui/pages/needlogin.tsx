06b22ebcd43e0c6a53d26108ef0dbe282f657428
Ryota Nishi <ryota0312@s.okayama-u.ac.jp>
2021-11-19 16:51:45 +0900

    Sample login necessary page

---
+++ ui/pages/needlogin.tsx
@@ -0,0 +1,26 @@
+import Head from 'next/head'
+import React from 'react'
+import useSWR from 'swr'
+import Layout from '../components/layout'
+import { useApi } from '../hooks/useApi'
+
+const SampleLoginNecessaryPage: React.FC = () => {
+  const { fetcher } = useApi()
+  const { data, error } = useSWR(['/auth/check', true], fetcher)
+
+  if (error) return <div>failed to load</div>
+  if (!data) return <div>loading...</div>
+
+  return (
+    <Layout>
+      <Head>
+        <title>Stella Finder</title>
+      </Head>
+
+      <main>
+        <h2>このページはログインが必要です</h2>
+      </main>
+    </Layout>
+  )
+}
+export default SampleLoginNecessaryPage

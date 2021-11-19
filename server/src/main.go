b4a35ff998a00154a9e3cc6598f4bb5e1bab5cd3
Ryota Nishi <ryota0312@s.okayama-u.ac.jp>
2021-11-19 14:45:19 +0900

    WIP 未ログインユーザにページを表示しない

--- server/src/main.go
+++ server/src/main.go
@@ -8,6 +8,7 @@ import (
 	"net/http"
 	"net/http/httputil"
 	"net/url"
+	"regexp"
 	"stella-finder-server/src/controller"
 )
 
@@ -51,6 +52,7 @@ func serve() {
 	}
 
 	// Proxy to Next.js
+	router.Use(sessionCheckFallbackLoginPage())
 	router.NoRoute(ReverseProxy)
 
 	if err := router.Run(":3001"); err != nil {
@@ -74,6 +76,26 @@ func ReverseProxy(c *gin.Context) {
 
 func sessionCheck() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		session := sessions.Default(c)
+		mailAddress := session.Get("mailAddress")
+
+		if mailAddress == nil {
+			// 認証に失敗した場合の処理はフロント側で行う
+			c.JSON(http.StatusUnauthorized, "Unauthorized.")
+			c.Abort()
+		} else {
+			c.Next()
+		}
+	}
+}
+
+func sessionCheckFallbackLoginPage() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		r := regexp.MustCompile(`^(.*(/login|_next.*|\..+)$).*$`)
+		if r.MatchString(c.Request.URL.String()) {
+			c.Next()
+		}
+
 		session := sessions.Default(c)
 		mailAddress := session.Get("mailAddress")
 

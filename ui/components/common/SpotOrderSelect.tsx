460d88046b7d6793cbcbb1c6f24472efa0cf67ad
Ryota Nishi <ryota0312@s.okayama-u.ac.jp>
2021-12-11 15:28:39 +0900

    Add sort param to search component

--- ui/components/common/SpotOrderSelect.tsx
+++ ui/components/common/SpotOrderSelect.tsx
@@ -16,8 +16,14 @@ export const SpotOrderSelect: React.FC<SpotOrderSelectProps> = (
 ) => {
   const orders = [
     { key: 'avg_total_point+desc', displayValue: '総合評価が高い順' },
-    { key: 'avg_total_point+asc', displayValue: '総合評価が低い順' },
+    { key: 'avg_darkness_point+desc', displayValue: '空の暗さの評価が高い順' },
+    { key: 'avg_view_point+desc', displayValue: '見晴らしの評価が高い順' },
+    { key: 'avg_safety_point+desc', displayValue: '安全性の評価が高い順' },
     { key: 'review_count+desc', displayValue: '口コミが多い順' },
+    { key: 'avg_total_point+asc', displayValue: '総合評価が低い順' },
+    { key: 'avg_darkness_point+asc', displayValue: '空の暗さの評価が低い順' },
+    { key: 'avg_view_point+asc', displayValue: '見晴らしの評価が低い順' },
+    { key: 'avg_safety_point+asc', displayValue: '安全性の評価が低い順' },
     { key: 'review_count+asc', displayValue: '口コミが少ない順' },
   ] as Array<OrderKey>
 

38f85b8beb4db0f81a8b4e556ecda894dddba57c
Ryota Nishi <ryota0312@s.okayama-u.ac.jp>
2021-12-22 07:29:23 +0900

    Remove unused import

--- ui/pages/article/list.tsx
+++ ui/pages/article/list.tsx
@@ -1,14 +1,5 @@
 import React from 'react'
-import { toast } from 'react-toastify'
-import { useRouter } from 'next/router'
-import useSWR from 'swr'
 import Layout from '../../components/layout'
-import { InputField } from '../../components/common/InputField'
-import { useStateWithValidate } from '../../hooks/useStateWithValidate'
-import { TextField } from '../../components/common/TextField'
-import { useApi } from '../../hooks/useApi'
-import { Loading } from '../../components/common/Loading'
-import { ArticleListWidget } from '../../components/article/ArticleListWidget'
 import { ArticleList } from '../../components/article/ArticleList'
 
 const List: React.FC = () => {

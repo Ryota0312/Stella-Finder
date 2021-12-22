6268963a12cf9c76234ca1911816b90a02467aa8
Ryota Nishi <ryota0312@s.okayama-u.ac.jp>
2021-12-22 15:44:54 +0900

    Fix deploy workflow

--- scripts/prod/migrate-db.sh
+++ scripts/prod/migrate-db.sh
@@ -4,7 +4,7 @@ if (git diff HEAD~ --name-only | grep db/mysql_init); then
   SQL_ARRAY=$(git diff HEAD~ --name-only | grep db/mysql_init)
   for sql in "${SQL_ARRAY[@]}"
   do
-      scripts/prod/docker-compose-wrapper.sh exec stella-finder-db mysql --defaults-extra-file=/etc/mysql/conf.d/dbaccess.conf -e"$(cat "$sql")"
+      scripts/prod/docker-compose-wrapper.sh exec -T stella-finder-db mysql --defaults-extra-file=/etc/mysql/conf.d/dbaccess.conf -e"$(cat "$sql")"
   done
 else
   echo "Skip migration"

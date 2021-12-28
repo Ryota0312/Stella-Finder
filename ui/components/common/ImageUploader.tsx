9ddce2756721f3e54a077b409fcea2ca4097d9b2
Ryota Nishi <ryota0312@s.okayama-u.ac.jp>
2021-12-28 23:43:28 +0900

    Fix design ImageUploader

--- ui/components/common/ImageUploader.tsx
+++ ui/components/common/ImageUploader.tsx
@@ -94,7 +94,7 @@ const LoadingOverlay = styled.div`
   width: 100%;
   height: 100%;
   background-color: rgba(255, 255, 255, 0.3);
-  position: absolute;
+  position: fixed;
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);

89c8576d9e1d9fb5eeced8d8b71b48f9948dec1c
Ryota Nishi <ryota0312@s.okayama-u.ac.jp>
2022-01-12 21:14:01 +0900

    Add about link to footer

--- ui/components/footer/Footer.tsx
+++ ui/components/footer/Footer.tsx
@@ -1,25 +1,33 @@
 import React from 'react'
 import styled from 'styled-components'
 import Image from 'next/image'
+import Link from 'next/link'
 
 export const Footer: React.FC = () => {
   return (
     <LayoutFooter>
-      <Twitter>
-        <a
-          href="https://twitter.com/stella_finder"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            src="/image/twitter-logo.png"
-            alt="twitter"
-            width={32}
-            height={26}
-          />
-        </a>
-      </Twitter>
-      <CopyRight>Copyright © 2022 Ryota Nishi All Rights Reserved.</CopyRight>
+      <Links>
+        <FooterLink>
+          <Link href="/about">Stella Finderについて</Link>
+        </FooterLink>
+        <Twitter>
+          <a
+            href="https://twitter.com/stella_finder"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+              src="/image/twitter-logo.png"
+              alt="twitter"
+              width={32}
+              height={26}
+            />
+          </a>
+        </Twitter>
+      </Links>
+      <CopyRight>
+        Copyright © 2021-2022 Ryota Nishi All Rights Reserved.
+      </CopyRight>
     </LayoutFooter>
   )
 }
@@ -34,11 +42,27 @@ const LayoutFooter = styled.footer`
   }
 `
 
+const Links = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: flex-start;
+  gap: min(138px, 30%);
+  margin-bottom: 8px;
+`
+
 const Twitter = styled.div`
   color: white;
   font-size: 10px;
-  text-align: center;
-  margin-bottom: 8px;
+`
+
+const FooterLink = styled.div`
+  font-size: 10px;
+
+  a {
+    &:hover {
+      text-decoration: underline;
+    }
+  }
 `
 
 const CopyRight = styled.div`

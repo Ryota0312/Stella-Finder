89dd7d2bc3b38595f9ef010bff9ac319acfd4811
Ryota Nishi <ryota0312@s.okayama-u.ac.jp>
2022-01-12 20:51:18 +0900

    Add about page

---
+++ ui/pages/about.tsx
@@ -0,0 +1,21 @@
+import Head from 'next/head'
+import React from 'react'
+import Layout from '../components/layout'
+
+const About: React.FC = () => {
+  return (
+    <Layout>
+      <Head>
+        <title>
+          Stella Finderについて | 天体観測スポットの検索・情報共有コミュニティ
+        </title>
+      </Head>
+
+      <main>
+        <h2>Stella Finderについて</h2>
+        TBW...
+      </main>
+    </Layout>
+  )
+}
+export default About

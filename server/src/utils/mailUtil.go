3051c47adc7023c69dc46578154565747ba7bc3d
Ryota Nishi <ryota0312@s.okayama-u.ac.jp>
2022-04-15 22:53:43 +0900

    wip impl server

--- server/src/utils/mailUtil.go
+++ server/src/utils/mailUtil.go
@@ -54,3 +54,45 @@ func SendTmpRegisterMail(toMailAddress string, registerKey string) {
 		fmt.Println(response.Headers)
 	}
 }
+
+func SendContactMail(fromMailAddress string, body string) {
+	loadErr := godotenv.Load()
+	if loadErr != nil {
+		log.Fatalf("error: %v", loadErr)
+	}
+
+	// .envから環境変数読み込み
+	API_KEY := os.Getenv("API_KEY")
+	FROM := os.Getenv("FROM")
+	CONTACT_TO := os.Getenv("CONTACT_TO")
+
+	// メッセージの構築
+	message := mail.NewV3Mail()
+	// 送信元を設定
+	from := mail.NewEmail("", FROM)
+	message.SetFrom(from)
+
+	// 1つ目の宛先と、対応するSubstitutionタグを指定
+	p := mail.NewPersonalization()
+	to := mail.NewEmail("", CONTACT_TO)
+	p.AddTos(to)
+	message.AddPersonalizations(p)
+
+	// 件名を設定
+	message.Subject = "【Stella Finder】お問い合わせです"
+	// テキストパートを設定
+	c := mail.NewContent("text/plain", "FROM: "+fromMailAddress+"\r\n"+
+		"本文: "+body+"\r\n")
+	message.AddContent(c)
+
+	// メール送信を行い、レスポンスを表示
+	client := sendgrid.NewSendClient(API_KEY)
+	response, err := client.Send(message)
+	if err != nil {
+		log.Println(err)
+	} else {
+		fmt.Println(response.StatusCode)
+		fmt.Println(response.Body)
+		fmt.Println(response.Headers)
+	}
+}

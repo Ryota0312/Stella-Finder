663334e01d83fef9e0741f7fba37a966b684c061
Ryota Nishi <ryota0312@s.okayama-u.ac.jp>
2022-07-07 11:57:20 +0900

    Rename method and remove unless debug line

--- server/src/controller/moonPhaseController.go
+++ server/src/controller/moonPhaseController.go
@@ -21,7 +21,7 @@ type MoonAgeOutputForm struct {
 	MoonAge float64 `json:"moon_age"`
 }
 
-func GetMoonAgeGrpc(c *gin.Context) {
+func GetMoonAge(c *gin.Context) {
 	jst, err := time.LoadLocation("Asia/Tokyo")
 	if err != nil {
 		panic(err)
@@ -40,7 +40,6 @@ func GetMoonAgeGrpc(c *gin.Context) {
 	defer conn.Close()
 	gc := moon.NewMoonApiClient(conn)
 
-	// Echo メソッドを呼び出す
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
 	defer cancel()
 	r, err := gc.MoonInfo(ctx, &moon.MoonInfoRequest{
@@ -54,56 +53,8 @@ func GetMoonAgeGrpc(c *gin.Context) {
 	if err != nil {
 		log.Fatalf("Could not echo: %v", err)
 	}
-	log.Printf("Received from server: %f", r.GetMoonAge())
 
 	output.MoonAge = r.GetMoonAge()
-
-	c.JSON(http.StatusOK, output)
-}
-
-func GetMoonAge(c *gin.Context) {
-	cacheDb := utils.NewCacheDb()
-	defer cacheDb.CloseCacheDb()
-
-	jst, err := time.LoadLocation("Asia/Tokyo")
-	if err != nil {
-		panic(err)
-	}
-	time.Local = jst
-
-	today := time.Now()
-	const layout = "2006-01-02"
-	todayString := today.Format(layout)
-
-	var output MoonAgeOutputForm
-
-	cache, err := cacheDb.Get(todayString)
-	if cache == nil {
-		response, _ := http.Get("https://labs.bitmeister.jp/ohakon/json/?mode=moon_age" +
-			"&year=" + strconv.Itoa(today.Year()) +
-			"&month=" + strconv.Itoa(int(today.Month())) +
-			"&day=" + strconv.Itoa(today.Day()))
-		responseBody, _ := ioutil.ReadAll(response.Body)
-		defer response.Body.Close()
-
-		if err := json.Unmarshal(responseBody, &output); err != nil {
-			c.JSON(http.StatusInternalServerError, "Internal server error")
-			return
-		}
-
-		err = cacheDb.Set(todayString, responseBody, 60*60*24)
-		if err != nil {
-			c.JSON(http.StatusInternalServerError, "Cache error")
-			return
-		}
-
-	} else {
-		err = json.Unmarshal(cache, &output)
-		if err != nil {
-			c.JSON(http.StatusInternalServerError, "Cache error")
-			return
-		}
-	}
 
 	c.JSON(http.StatusOK, output)
 }

76b80f4de4e962178001f07510743c7f21c61ea3
Ryota Nishi <ryota0312@s.okayama-u.ac.jp>
2022-07-09 21:44:05 +0900

    Fix backup.service

--- scripts/prod/backup/setup-daily-backup.sh
+++ scripts/prod/backup/setup-daily-backup.sh
@@ -2,7 +2,8 @@
 
 SCRIPT_DIR=$(dirname $0)
 
-cp "$SCRIPT_DIR"/backup.service /etc/systemd/system/
+cp "$SCRIPT_DIR"/backup.service.tmp /etc/systemd/system/backup.service
+sed -i -e "s|?HOME_DIR|$HOME|g" /etc/systemd/system/backup.service
 cp "$SCRIPT_DIR"/backup.timer /etc/systemd/system/
 
 systemctl start backup.timer
